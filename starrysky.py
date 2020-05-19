3a67516a65b65a967b9aed23087cc32b22d5e19b
mixl5448 <mixl5448@gmail.com>
2020-05-18 23:02:28 -0700

    Started Starry Sky program

---
+++ starrysky.py
@@ -0,0 +1,14 @@
+import pygame 
+
+class StarrySky: 
+	def __init__(self): 
+		pygame.init()
+
+	def run_game(self): 
+		#while True: 
+
+
+
+if __name__ is "__main__": 
+	ss = StarrySky()
+	ss.run_game()
