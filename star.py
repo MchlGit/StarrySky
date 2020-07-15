399ccaf4e33be87302b730d51daa9746d5e8d5b8
mixl5448 <mixl5448@gmail.com>
2020-07-14 22:49:20 -0700

    Created grid of stars

--- star.py
+++ star.py
@@ -1,14 +1,19 @@
 import pygame
 from pygame.sprite import Sprite
 
-class Start(Sprite): 
+class Star(Sprite): 
 	def __init__(self, starry_night): 
+
+		super().__init__()
 		self.screen = starry_night.screen
 		self.screen_rect = self.screen.get_rect()
 
 		self.image = pygame.image.load('images/star.bmp')
 		self.rect = self.image.get_rect()
 
-		self.rect_x = self.rect.width
-		self.rect_y = self.rect.height
+		self.rect.x = self.rect.width
+		self.rect.y = self.rect.height
+
+		# store star's exact horizonal position
+		self.x = float(self.rect.x)
 		
--- starrysky.py
+++ starrysky.py
@@ -1,6 +1,7 @@
 import sys
 import pygame 
 from settings import Settings
+from star import Star
 
 class StarrySky: 
 	def __init__(self): 
@@ -8,6 +9,8 @@ class StarrySky:
 		self.settings = Settings()
 		self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption("Starry Sky")
+		self.stars = pygame.sprite.Group()
+		self._create_stars()
 
 	def run_game(self): 
 		while True: 
@@ -22,9 +25,29 @@ class StarrySky:
 
 	def _update_screen(self): 
 		self.screen.fill(self.settings.bg_color)
+		self.stars.draw(self.screen)
 		pygame.display.flip()
 
+	def _create_stars(self): 
+		star = Star(self)
+		star_width, star_height = star.rect.size
 
+		available_width = self.settings.screen_width - (2 * star_width)
+		num_stars = available_width // (2* star_width)
+		available_height = self.settings.screen_height - (2* star_height)
+		num_rows = available_height // (2 * star_height)
+
+		for row_number in range(num_rows): 
+			for star_number in range(num_stars): 
+				self._create_star(star_number, row_number)
+
+	def _create_star(self, star_number, row_number): 
+		new_star = Star(self)
+		star_width, star_height = new_star.rect.size
+		new_star.x = star_width + (2 * star_width * star_number)
+		new_star.rect.x = int(new_star.x)
+		new_star.rect.y = star_height + (2 * star_height * row_number)
+		self.stars.add(new_star)
 
 if __name__ == '__main__': 
 	ss = StarrySky()
